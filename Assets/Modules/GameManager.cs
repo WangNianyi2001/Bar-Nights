2f670e1ac900641bbf6d731de39087cfe177b65c
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-11-17 07:16:51 +0800

    Fix selector key using by adapting to input system actions

--- Assets/Modules/GameManager.cs
+++ Assets/Modules/GameManager.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
+using PixelCrushers;
 using PixelCrushers.DialogueSystem;
+using UnityEngine.InputSystem;
 using System;
-using System.Collections;
 
 namespace Game {
 	public class GameManager : MonoBehaviour {
@@ -14,12 +15,13 @@ namespace Game {
 
 		#region Inspector fields
 		new public Camera camera;
-		public enum Status { None, Dialogue, Bartending, Customer, Entering }
-		public Status start = Status.None;
+		public enum State { None, Dialogue, Bartending, Customer, Entering }
+		public State start = State.None;
+		public InputActionAsset actions;
 		#endregion
 
 		#region Core 
-		Status status;
+		State state;
 		[NonSerialized] public ViewManager view;
 		[NonSerialized] public BartendingManager bartending;
 		[NonSerialized] public DialogueSystemController dialogue;
@@ -27,26 +29,26 @@ namespace Game {
 		#endregion
 
 		#region Public interfaces
-		public void SwitchTo(Status value) {
-			if(status == value)
+		public void SwitchTo(State value) {
+			if(state == value)
 				return;
 			bartending.Active = false;
 			switch(value) {
-				case Status.None:
+				case State.None:
 					view.Deactivate();
 					ui.Deactivate();
 					break;
-				case Status.Dialogue:
+				case State.Dialogue:
 					view.SwitchTo(view.dialogue);
 					ui.SwitchTo(ui.dialogue);
 					break;
-				case Status.Bartending:
+				case State.Bartending:
 					view.SwitchTo(view.bartending);
 					ui.SwitchTo(ui.bartending);
 					bartending.Active = true;
 					break;
 			}
-			status = value;
+			state = value;
 		}
 		#endregion
 
@@ -59,6 +61,7 @@ namespace Game {
 		}
 
 		void Start() {
+			InputDeviceManager.RegisterInputAction("Use", actions.FindAction("Use"));
 			SwitchTo(start);
 		}
 		#endregion
